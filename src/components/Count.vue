1bcb1a43ae772bc8c26d152ba670b385d2ac202c
李世星 <70449@any3.com>
2019-03-20 16:16:05 +0800

    add props test

--- src/components/Count.vue
+++ src/components/Count.vue
@@ -3,7 +3,7 @@
         <h2>{{msg}}</h2>
         <hr/>
         <h3>{{this.$store.state.count}}</h3>
-        <h3>{{count}}</h3>
+        <h3>{{testCount}}</h3>
 
         <div>
             <button @click="add(10)">add</button>
@@ -13,17 +13,21 @@
             <button @click="addAction">addAction</button>
             <button @click="reduceAction">reduceAction</button>
         </div>
+        <vchild :message="message"></vchild>
     </div>
 </template>
 
 <script>
     import store from '@/vuex/store'
     import {mapState, mapMutations, mapGetters, mapActions} from 'vuex';
+    import vchild from "./VChild";
     export default {
+        components: {vchild},
         name: "Count",
         data() {
             return {
                 msg: 'Hello Vuex',
+                message:"Hello child"
             }
         },
         store,
@@ -31,9 +35,14 @@
             ...mapMutations(['add', 'reduce']),
             ...mapActions(['addAction', 'reduceAction'])
         },
+        // computed:{
+        //     ...mapState(["count"]),
+        //    // ...mapGetters(["count"]),
+        // }
         computed:{
-            ...mapState(["count"]),
-           // ...mapGetters(["count"]),
+            testCount:function () {
+                return this.$store.state.count;
+            }
         }
     }
 </script>
---
+++ src/components/VChild.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p>我是子组件，接收来自父组件的信息:{{message}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "vchild",
+        props:['message']
+    }
+</script>
+
+<style scoped>
+
+</style>
